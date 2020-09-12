c93fdb7296ad674b44762c4566e82d17d661966d
doggogqgqgqgqgqgqgqgqgqg <68341606+doggogqgqgqgqgqgqgqgqgqg@users.noreply.github.com>
2020-09-12 15:51:19 +0100

    Update config.php

--- captures/config.php
+++ captures/config.php
@@ -3,9 +3,9 @@
 return array(
     'secure_key' => 'Ding-Ding-Uploader',
 
-    'output_url' => 'http://yourdomain.com/captures/',
+    'output_url' => 'http://img.ding-ding-uploader.tk/captures/',
 
-    'redirect_url' => 'http://yourdomain.com/captures/',
+    'redirect_url' => 'http://img.ding-ding-uploader.tk/captures/',
 
     'allowed_ips' => array(),
 
